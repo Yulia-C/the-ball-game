ed2e8667e748500b176c0b33c564205f07544bb6
Yulia C <yulka1355@gmail.com>
2025-03-05 11:48:38 +0200

    Implement on click size increase

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 'use strict'
 
 function onBallClick() {
-    console.log('Ball Clicked!')
+    const elBall = document.querySelector('.ball')
+    var currWidth = parseInt(elBall.style.width) || 100
+    var currHeight = parseInt(elBall.style.height) || 100
+
+    var newWidth = currWidth + 50
+    var newHeight = currHeight + 50
+
+    elBall.style.width = `${newWidth}px`
+    elBall.style.height = `${newHeight}px`
+    
+    elBall.innerText = newHeight + newWidth
  
 }
