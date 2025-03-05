e247bce783f62d0c8f145689f876debddc82cfe5
Yulia C <yulka1355@gmail.com>
2025-03-05 12:26:05 +0200

    Add ball size diameter reset

--- js/main.js
+++ js/main.js
@@ -7,10 +7,18 @@ function onBallClick() {
 
     var newWidth = currWidth + 50
     var newHeight = currHeight + 50
+    var elDiameter = newWidth + newHeight
+
 
     elBall.style.width = `${newWidth}px`
     elBall.style.height = `${newHeight}px`
-    
+
     elBall.innerText = newHeight + newWidth
- 
+
+
+    if (elDiameter > 400) {
+        elBall.style.width = 100 + `px`
+        elBall.style.height = 100 + `px`
+        elBall.innerText = 100
+    }
 }
