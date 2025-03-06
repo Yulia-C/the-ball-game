e183077ffc450e3147cecf1d3320b813a55e877f
Yulia C <yulka1355@gmail.com>
2025-03-06 09:22:14 +0200

    implement getRandomColor() to ball

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
+    const randSize = getRandomInt(20,60)
+    const randColor = getRandomColor()
+    elBall.style.backgroundColor = randColor
+    
     var currWidth = parseInt(elBall.style.width) || 100
     var currHeight = parseInt(elBall.style.height) || 100
-    const randSize = getRandomInt(20,60)
 
     var newWidth = currWidth + randSize
     var newHeight = currHeight + randSize
